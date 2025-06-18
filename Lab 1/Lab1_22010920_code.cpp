24b1bfbd92393327ee256c64e9cecaa6a8fd2f6e
levabellee <levabelleedu@gmail.com>
2025-06-18 08:57:43 +0800

    Add files via upload

---
+++ Lab 1/Lab1_22010920_code.cpp
@@ -0,0 +1,40 @@
+/* Name: Lez Evabelle Kennedy
+Student ID: 22010920
+Lab No.: 1*/
+
+#include <iostream>
+#include <string>
+using namespace std;
+
+struct Student {
+    string name;
+    int id;
+    string email;
+    string phone;
+};
+
+int main() {
+    Student student[5];
+    
+    for (int i=1; i<5+1; i++) {
+        cout << "Enter ID for Student #" << i << ": ";
+        cin >> student[i].id;
+        cout << "Enter Name for Student #" << i << ": ";
+        cin >> student[i].name;
+        cout << "Enter Email for Student #" << i << ": ";
+        cin >> student[i].email;
+        cout << "Enter Phone for Student #" << i << ": ";
+        cin >> student[i].phone;
+        cout <<"============================" << endl;
+    }
+    
+    for (int i = 0; i < 5; i++) {
+        cout <<"========Student Info========"<<endl;
+        cout << "ID: "<< student[i].id << endl;
+        cout << "Name: "<< student[i].name << endl;
+        cout << "Contact Num.: "<< student[i].name << endl;
+        cout << "Email: "<< student[i].name << endl;
+        cout << "============================" << endl;
+    }
+    return 0;
+}
